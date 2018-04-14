6386cc42609a287faa0e7c891ced5a007dfcb289
arvalon <me@arvalon.ru>
2018-04-14 12:36:08 +0300

    Upgrade!

--- app/src/main/java/ru/arvalon/mytraining/MainActivity.java
+++ app/src/main/java/ru/arvalon/mytraining/MainActivity.java
@@ -1,12 +1,9 @@
 package ru.arvalon.mytraining;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import ru.arvalon.mytraining.GymDbActivities.AvaliableEquipmentsActivities.EquipmentsActivity;
 import ru.arvalon.mytraining.GymDbActivities.GymDB;
@@ -17,56 +14,28 @@ import ru.arvalon.mytraining.dialogs.DialogActivationDB;
 
 public class MainActivity extends AppCompatActivity implements DialogActivationDB.DialogHost {
 
-    private Button mytrainers;
-    private Button myprogramm;
-    private Button workout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        super.closeContextMenu();
-
-        mytrainers=(Button)findViewById(R.id.mytrainers);
-        workout=(Button)findViewById(R.id.workout);
-        myprogramm=(Button)findViewById(R.id.myprogramm);
-
-        mytrainers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO Dialog
-                startMyTrainersActivity();
-
-            }
-        });
-        myprogramm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startMyProgrammActivity();
-            }
-        });
-        workout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                workout();
-            }
-        });
+        findViewById(R.id.mytrainers).setOnClickListener(v->{startMyTrainersActivity();});
+        findViewById(R.id.workout).setOnClickListener(v->{startMyProgrammActivity();});
+        findViewById(R.id.myprogramm).setOnClickListener(v->{workout();});
     }
 
 
     private void startMyProgrammActivity() {
-        Log.d("norm","Button MyProgrammActivity is pressed");
+        Log.d(App.TAG,"Button MyProgrammActivity is pressed");
         Intent myprogrammintent=new Intent(this,MyProgrammActivity.class);
         startActivity(myprogrammintent);
     }
 
     private void workout() {
-        Log.d("norm","Button WorkoutActivity is pressed");
+        Log.d(App.TAG,"Button WorkoutActivity is pressed");
         Intent workoutintent=new Intent(this,WorkoutActivity.class);
         startActivity(workoutintent);
     }
-
 
     public void startMyTrainersActivity(){
         DatabaseAccess databaseAccess=DatabaseAccess.getInstance(this);
